84233cd38dac6bcafa7d68f030c05da080789f8e
Kevin Kenyon <kvkenyon@gmail.com>
2016-07-13 23:38:40 -0500

    Add practice code for lecture 2

--- practice/functions.hs
+++ practice/functions.hs
@@ -30,3 +30,25 @@ sumEveryTwo [] = []
 sumEveryTwo (x:[]) = [x]
 sumEveryTwo (x:(y:zx)) = (x + y) : sumEveryTwo(zx)
 
+data Thing = Ship
+           | SealingWax
+           | King
+           | Shoe
+           | Cabbage
+  deriving Show
+
+type Name = String
+type Age = Integer
+
+data Person = Person Name Age Thing
+  deriving Show
+
+getAge :: Person -> Integer
+getAge (Person _ a _) = a
+
+showPerson :: Person -> String
+showPerson p@(Person n a t) = "name = " ++ n ++ " age = " ++ show a ++ " thing = " ++ show t ++ " for " ++ show p
+
+whatsYourAge :: Person -> String
+whatsYourAge (Person _ 27 _ ) = "That's my age mate."
+whatsYourAge (Person _ _ _) = "Lame age"
