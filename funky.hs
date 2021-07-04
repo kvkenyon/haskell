5483d7041b5642055b8a0f0cf583fec6198ee71e
Kevin Kenyon <kvkenyon@gmail.com>
2021-07-03 20:50:07 -0500

    trie preorder thuggery

--- funky.hs
+++ funky.hs
@@ -31,3 +31,32 @@ hasPath (x:xs) u v
     | u == v = True
     | fst x == u = hasPath xs (snd x) v
     | otherwise = or  [hasPath xs v' v | (u', v') <- xs, u' == u] 
+
+rev :: [a] -> [a]
+rev = foldl (\acc x -> x:acc) [] 
+
+prefixes :: [a] -> [[a]]
+prefixes xs = rev (foldl (\acc x -> (aux x acc):acc) [] xs)
+    where 
+        aux x [] = [x]
+        aux x acc = (head acc) ++ [x]
+
+prefixes2 :: [a] -> [[a]]
+prefixes2 = foldr (\x acc -> [x] : (map ((:) x))  acc) [] 
+
+lagrange :: [(Float, Float)] -> Float -> Float
+lagrange points x = foldl (\acc (xj, yj) -> acc + yj * (l x xj points)) 0 points
+
+l :: Float -> Float -> [(Float,Float)] -> Float
+l x xj points = foldl (\acc (x',y') -> if xj /= x' 
+    then acc * ((x - x') / (xj - x')) else acc * 1) 1 points 
+
+data Trie a = Leaf a | Node a [Trie a]
+foldtrie :: (b -> a -> b) -> b -> Trie a -> b
+foldtrie f acc (Leaf a) = f acc a
+foldtrie f acc (Node a t) =  foldl (\acc x -> foldtrie f acc x) (f acc a) t
+
+    
+
+
+
